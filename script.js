1b94f55e35171fb0fce4477532b6687b68c3414c
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-09-30 20:41:52 -0500

    Changed button icons to prevent issues on click event. Added click listener to save text input

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-currentTime = 16//moment().format("H");
-// console.log(currentTime)
+currentTime = moment().format("H");
 var textArea=$(".text-area")
-console.log(textArea)
+// console.log(textArea)
 window.setInterval(function () {
     $('#currentDay').html(moment().format('ddd MM/DD/YYYY H:mm:ss'))
 }, 1000);
-
+//
 for (i=0; i < textArea.length;  i++){
 
     if (parseInt(textArea[i].id) < currentTime){
+        // console.log(textArea[i])
         textArea[i].setAttribute('class','past');
     }
     else if (parseInt(textArea[i].id) === currentTime){
@@ -18,3 +18,13 @@ for (i=0; i < textArea.length;  i++){
         textArea[i].setAttribute('class','future')
     }
 }
+function saveItem(event) {
+   
+    var previousElement = $(event.target).prev()
+   var savedText = previousElement.val();
+//   localStorage.setItem('') 
+  console.log(previousElement)
+
+}
+$('.saveBtn').click(saveItem);
+
