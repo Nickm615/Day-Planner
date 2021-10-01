f79d1bcc86b1355d50f569471b4a28d47be0d99d
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-09-30 19:33:12 -0500

    Added logic to color text boxes

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+currentTime = 16//moment().format("H");
+// console.log(currentTime)
+var textArea=$(".text-area")
+console.log(textArea)
 window.setInterval(function () {
     $('#currentDay').html(moment().format('ddd MM/DD/YYYY H:mm:ss'))
 }, 1000);
 
+for (i=0; i < textArea.length;  i++){
+
+    if (parseInt(textArea[i].id) < currentTime){
+        textArea[i].setAttribute('class','past');
+    }
+    else if (parseInt(textArea[i].id) === currentTime){
+        textArea[i].setAttribute('class','present');
+    }
+    else if (parseInt(textArea[i].id) > currentTime) {
+        textArea[i].setAttribute('class','future')
+    }
+}
